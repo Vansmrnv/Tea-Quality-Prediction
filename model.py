5fa1ac66ff9b5c3463b599e753931d6cd293e400
Ivan Smirnov <150267611+Vansmrnv@users.noreply.github.com>
2024-07-16 23:12:09 +0300

    Update model.py

--- model.py
+++ model.py
@@ -4,17 +4,73 @@ from tensorflow.keras.layers import Dense
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, OneHotEncoder 
-from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer   
 import matplotlib.pyplot as plt
+from tensorflow.keras.utils import build_models
 
-model = Sequential([
-  Dense(units=8, activation='relu'),
-  Dense(units=8, activation='relu'),
-  Dense(units=1, activation='sigmoid')
-])
+# Load Data
+data = pd.read_csv('data/tea_datas.csv')
 
-model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate='1e-3'),
-              loss=tf.keras.losses.BinaryCrossentropy(from_logits=True))
+# Define features and target
+X = data[['water_temperature', 'steeping_time', 'milk', 'sugar']]
+y = data['tea_made_correctly']
 
-model.fit(X, Y, epochs=50)
+y = np.expand_dims(y, axis=1)
+
+# Split the data into training, validation, and test sets
+X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.4, random_state=42)
+X_cv, X_test, y_cv, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=1)
+
+del X_temp, y_temp
+
+print(f"the shape of the training set (input) is: {X_train.shape}")
+print(f"the shape of the training set (target) is: {y_train.shape}\n")
+print(f"the shape of the cross validation set (input) is: {X_cv.shape}")
+print(f"the shape of the cross validation set (target) is: {y_cv.shape}\n")
+print(f"the shape of the test set (input) is: {X_test.shape}")
+print(f"the shape of the test set (target) is: {y_test.shape}")
+
+scaler = StandardScaler()
+
+X_train_scaled = scaler.fit_transform(X_train)
+X_cv_scaled = scaler.fit_transform(X_cv)
+X_test_scaled = scaler.fit_transform(X_test)
+
+# Build the neural network model
+
+nn_train_error = []
+nn_cv_error = []
+
+models = tf.keras.utils.build_models()
+
+for model in models: 
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3),
+                  loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
+    
+    print(f'Training {model.name}...')
+
+    model.fit(X_train_scaled, y_train, epochs=50, verbose=0)
+
+    print('Done!\n')
+
+    threshold = 0.5
+
+    yhat = model.predict(X_train_scaled)
+    yhat = tf.math.sigmoid(yhat)
+    yhat = np.where(yhat >= threshold, 1, 0)
+    train_error = np.mean(yhat != y_train)
+    nn_train_error.append(train_error)
+
+    yhat = model.predict(X_cv_scaled)
+    yhat = tf.math.sigmoid(yhat)
+    yhat = np.where(yhat >= threshold, 1,0)
+    cv_error = np.mean(yhat != y_cv)
+    nn_cv_error.append(cv_error)
+
+for model_nem in range(len(nn_train_error)):
+    print(
+        f'Model {model_nem+1}: Training set Classification Error: {nn_train_error[model_nem]:.5f}, ' +
+        f'CV Set Classification Error: {nn_cv_error[model_nem]:.5f}'
+    )
